56fe64a832a36f719a2ca9d94166f93a3a3051b4
Moises Caraballo <moises.08.caraballo@gmail.com>
2024-02-05 12:03:04 +0100

    Subiendo archivos

---
+++ src/componentes/App.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Card from './Card.jsx'
+
+function App () {
+
+    const hoverAnimation = e => {
+        e.stopPropagation();
+        const card = e.target.closest('.card')
+        
+        const height = card.clientHeight
+        const width = card.clientWidth
+        const layerX = e.nativeEvent.layerX
+        const layerY = e.nativeEvent.layerY
+        const rotationY = ((layerX - width / 2) / width) * 20
+        const rotationX = ((layerY - height / 2) / height) * 20
+        const styles = `perspective(500px) scale(1.02) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`
+        
+        card.style.transform = styles
+    }
+
+    const removeAnimation = e => {
+        const card = e.target.closest('.card')
+        card.removeAttribute("style")
+    }
+
+    return (
+        <main>
+            <section>
+                <h1 className="title">
+                    Reliable, efficent delivery
+                    <span>Powered by Tecnology</span>
+                </h1>
+
+                <p className="description">Our Artificial Intelligence powered tools use millions of projects data points to ensure that your project is successful</p>
+
+                <div className="cards">
+                    <Card 
+                        title="supervisor"
+                        description="Monitors activity to identify projects roadblocks"
+                        image="supervisor"
+                        color="green"
+                        addHoverAnimation={hoverAnimation}
+                        removeHoverAnimation={removeAnimation}
+                    />
+                    <Card
+                        title="team builder"
+                        description="Scan our talent networkto create the optimal team for your project"
+                        image="team-builder"
+                        color="red"
+                        addHoverAnimation={hoverAnimation}
+                        removeHoverAnimation={removeAnimation}
+                    />
+                    <Card
+                        title="karma"
+                        description="Regulary evaluates our talent to ensure quality"
+                        image="karma"
+                        color="orange"
+                        addHoverAnimation={hoverAnimation}
+                        removeHoverAnimation={removeAnimation}
+                    />
+                    <Card
+                        title="calculator"
+                        description="Use data from past projects to provide better delivery estimates"
+                        image="calculator"
+                        color="blue"
+                        addHoverAnimation={hoverAnimation}
+                        removeHoverAnimation={removeAnimation}
+                    />
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default App
---
+++ src/componentes/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Card ({ title, description, image, color, addHoverAnimation, removeHoverAnimation }) {
+    return (
+        <article className={`card card--${color}`} onMouseMove={addHoverAnimation} onMouseLeave={removeHoverAnimation}>
+            <h2 className="card-title">{title}</h2>
+            <p className="card-description">{description}</p>
+
+            <figure className="card__figure">
+                <img src={`./images/icon-${image}.svg`} alt=" " aria-hidden="true" />
+            </figure>
+        </article>
+    )
+}
+
+export default Card
